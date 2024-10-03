1d1f20959cd1178d89b987a085885b9338b1b203
formesean <seanaguilar698@gmail.com>
2024-10-04 00:30:46 +0800

    change to catppuccin theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
-return {{
+return {
+  {
     "sainnhe/sonokai",
     priority = 1000,
     config = function()
-        vim.g.sonokai_transparent_background = "1"
-        vim.g.sonokai_enable_italic = "1"
-        vim.g.sonokai_style = "andromeda"
-        vim.cmd.colorscheme("sonokai")
-    end
-}}
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "andromeda"
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
